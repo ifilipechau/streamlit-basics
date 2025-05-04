1dc0884334928a20d85845dd89b7c97690aee70a
Filipe Chau <filipechau.dev@gmail.com>
2025-05-04 19:52:30 +0200

    refactor: improve comments and remove redundant text in hello-streamlit.py

--- hello-streamlit.py
+++ hello-streamlit.py
@@ -1,7 +1,9 @@
+# import the streamlit library
+# This library is used to creat web applications in python.
 import streamlit as st
 
 # st.write is a function in the Streamlit library that allows you to write text, data, and other content to the Streamlit app.
 st.write("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
 st.write("You can use this app to test your Streamlit installation.")
-st.write("You can also use this app to test your Python installation.")
+
