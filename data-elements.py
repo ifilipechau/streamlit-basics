90f0f42cb6777bc35662a60092d68ae20a34037d
Filipe Chau <filipechau.dev@gmail.com>
2025-05-05 12:52:07 +0200

    feat: add data-elements.py with dataframe examples and display it

---
+++ data-elements.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import pandas as pd
+
+# Set the title of the app
+st.title("Data Elements in Streamlit")
+
+# Dataframe creation
+st.subheader("Dataframe Example")
+df = pd.DataFrame({ 
+    'Name': ['Micas', 'Anselmo', 'Lorenzo', 'Charlie', 'Ester'],
+    'Age': [23, 45, 43, 28, 56],
+    'Occupation': ['Engineer', 'Doctor', 'Artist', 'Teacher', 'Phylosopher']
+})
+st.dataframe(df) # Display the dataframe with a scrollbar
